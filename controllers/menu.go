7d68d653b584ee8e0a0272ae2257fd083513ede7
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-02-19 23:58:10 +0700

    create menu controller

--- controllers/menu.go
+++ controllers/menu.go
@@ -1,7 +1,46 @@
 package controllers
 
+import (
+	"restaurant/models"
+	"restaurant/repository"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
 type MenuController interface {
+	CreateMenu(ctx *fiber.Ctx) error
 }
 
 type menuController struct {
+	menuRepository repository.MenuRepository
+}
+
+func NewMenuController(menuRepository repository.MenuRepository) MenuController {
+	return &menuController{menuRepository}
+}
+
+func (c *menuController) CreateMenu(ctx *fiber.Ctx) error {
+
+	body := &models.Menu{
+		ID:          uuid.New(),
+		Name:        "หมูสไลด์บาง",
+		Description: "หมู",
+		Image:       "deoaih",
+		Submenus: []models.Submenu{
+			{
+
+				ID:          uuid.New(),
+				Name:        "บาง",
+				Description: "บางมาก",
+				Image:       "sub",
+				Available:   1,
+			},
+		},
+		Available: 1,
+	}
+
+	c.menuRepository.CreateMenu(body)
+
+	return nil
 }
