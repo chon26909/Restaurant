e9d0fe4e6b282b241e7317251e384090a944e36f
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-02-19 23:59:22 +0700

    create model submenu

--- models/submenu.go
+++ models/submenu.go
@@ -1,12 +1,20 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Submenu struct {
-	ID          uuid.UUID `gorm:"primaryKey;column:id"`
-	MainMenuID  uuid.UUID
+	ID          uuid.UUID `gorm:"type:char(36);primary_key"`
+	MenuID      uuid.UUID `gorm:"type:char(36)"`
 	Name        string
 	Description string
 	Image       string
 	Available   int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
