1e71633e031cdf5d15111c738196a30a06d2c672
Chonlasit <chonlasit.s@arise.tech>
2023-02-24 18:11:23 +0700

    update main

--- main.go
+++ main.go
@@ -5,15 +5,11 @@ import (
 	"log"
 	"restaurant/controllers"
 	"restaurant/lib"
-	"restaurant/models"
 	"restaurant/repository"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
 	"github.com/spf13/viper"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
@@ -34,6 +30,8 @@ func main() {
 	menu := app.Group("/menu")
 	menu.Post("/", menuController.CreateMenu)
 	menu.Get("/", menuController.GetAllMenu)
+	menu.Put("/:id", menuController.UpdateMenu)
+	menu.Delete("/:id", menuController.DeleteMenu)
 
 	log.Fatal(app.Listen(fmt.Sprintf(":%v", viper.GetInt("app.port"))))
 }
@@ -52,55 +50,55 @@ func initConfig() {
 	}
 }
 
-func Test() {
+// func Test() {
 
-	// Connect to the MySQL database
-	dsn := "restaurant:P@ssw0rd@tcp(127.0.0.1:3306)/restaurant?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: &lib.SqlLogger{},
-	})
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
+// 	// Connect to the MySQL database
+// 	dsn := "restaurant:P@ssw0rd@tcp(127.0.0.1:3306)/restaurant?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+// 		Logger: &lib.SqlLogger{},
+// 	})
+// 	if err != nil {
+// 		log.Fatalf("Failed to connect to database: %v", err)
+// 	}
 
-	// Auto-migrate the models
-	err = db.AutoMigrate(&models.User{}, &models.Email{})
-	if err != nil {
-		log.Fatalf("Failed to auto-migrate models: %v", err)
-	}
+// 	// Auto-migrate the models
+// 	err = db.AutoMigrate(&models.User{}, &models.Email{})
+// 	if err != nil {
+// 		log.Fatalf("Failed to auto-migrate models: %v", err)
+// 	}
 
-	// Create a sample user and emails
-	user := models.User{
-		ID:   uuid.New(),
-		Name: "John Doe",
-		Emails: []models.Email{
-			{
-				ID:      uuid.New(),
-				Address: "john.doe@example.com",
-				Status:  "false",
-			},
-			{
-				ID:      uuid.New(),
-				Address: "johndoe@gmail.com",
-				Status:  "false",
-			},
-		},
-	}
+// 	// Create a sample user and emails
+// 	user := models.User{
+// 		ID:   uuid.New(),
+// 		Name: "John Doe",
+// 		Emails: []models.Email{
+// 			{
+// 				ID:      uuid.New(),
+// 				Address: "john.doe@example.com",
+// 				Status:  "false",
+// 			},
+// 			{
+// 				ID:      uuid.New(),
+// 				Address: "johndoe@gmail.com",
+// 				Status:  "false",
+// 			},
+// 		},
+// 	}
 
-	// Insert the user and emails into the database
-	err = db.Create(&user).Error
-	if err != nil {
-		log.Fatalf("Failed to insert user and emails: %v", err)
-	}
+// 	// Insert the user and emails into the database
+// 	err = db.Create(&user).Error
+// 	if err != nil {
+// 		log.Fatalf("Failed to insert user and emails: %v", err)
+// 	}
 
-	// Print the user and email data from the database
-	var userFromDB models.User
-	err = db.Preload("Emails").First(&userFromDB, "name = ?", "John Doe").Error
-	if err != nil {
-		log.Fatalf("Failed to get user from database: %v", err)
-	}
-	fmt.Printf("User: %v\n", userFromDB)
-	for _, email := range userFromDB.Emails {
-		fmt.Printf("Email: %v\n", email)
-	}
-}
+// 	// Print the user and email data from the database
+// 	var userFromDB models.User
+// 	err = db.Preload("Emails").First(&userFromDB, "name = ?", "John Doe").Error
+// 	if err != nil {
+// 		log.Fatalf("Failed to get user from database: %v", err)
+// 	}
+// 	fmt.Printf("User: %v\n", userFromDB)
+// 	for _, email := range userFromDB.Emails {
+// 		fmt.Printf("Email: %v\n", email)
+// 	}
+// }
