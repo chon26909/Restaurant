242f9986e7abbd9a5c77a76074b2d1997c3ca980
Chonlasit <chonlasit.s@arise.tech>
2023-02-24 18:11:43 +0700

    create menu dto

--- dto/menu.go
+++ dto/menu.go
@@ -1,4 +1,25 @@
 package dto
 
+import "github.com/google/uuid"
+
+type Submenu struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Available   bool   `json:"available"`
+}
+
 type CreateMenuRequest struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	Submenus    []Submenu
+	Available   bool `json:"available"`
+}
+
+type MenuResponse struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Image     string    `json:"image"`
+	Submenus  []Submenu `json:"submenus"`
+	Available bool      `json:"available"`
 }
