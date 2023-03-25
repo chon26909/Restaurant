0b3915a08f3cbbb1aacd6ed36da2a99b3370e74a
HiChon <59905927+chon26909@users.noreply.github.com>
2023-03-25 23:28:15 +0700

    update

--- models/user.go
+++ models/user.go
@@ -3,10 +3,12 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
+	ID        uuid.UUID `gorm:"type:char(36);primaryKey"`
 	Email     string
 	Password  string
 	Fistname  string
